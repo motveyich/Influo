deb26a2764e1e4575f2be167026b5baf25081ae2
motveyich <motveyich@gmail.com>
2025-09-12 22:58:08 +0300

    Fix applicationId is not defined error in OffersPage

--- src/modules/offers/components/OffersPage.tsx
+++ src/modules/offers/components/OffersPage.tsx
@@ -881,7 +881,7 @@ export function OffersPage() {
                     onChange={(e) => setSearchQuery(e.target.value)}
                     className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-purple-500 focus:border-transparent"
                   />
-                </div>
+                const dealId = offer.metadata?.dealId || (offer as any).id || null;
                 
                 <select
                   value={selectedFilter}
