aa9bc9b5931770c715300f8330325dc2f5e5d9c6
motveyich <motveyich@gmail.com>
2025-09-12 23:29:43 +0300

    Updated OffersPage.tsx

--- src/modules/offers/components/OffersPage.tsx
+++ src/modules/offers/components/OffersPage.tsx
@@ -323,6 +323,8 @@ export function OffersPage() {
 
         {/* Offers Grid */}
         {isLoading ? (
+            )
+            }
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
             {[...Array(4)].map((_, i) => (
               <div key={i} className="bg-white rounded-lg shadow-md p-6 animate-pulse">
@@ -331,3 +333,8 @@ export function OffersPage() {
                     <div className="h-6 bg-gray-300 rounded mb-2"></div>
                     <div className="h-4 bg-gray-300 rounded w-1/3 mb-2"></div>
                 
+          )
+          )
+          }
+  )
+}
