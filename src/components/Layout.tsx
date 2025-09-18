7ff57a84257206041ef290d0fae14116c5ea11f1
motveyich <motveyich@gmail.com>
2025-09-18 03:27:09 +0300

    Fix Settings icon import error

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,7 +9,8 @@ import {
   Menu,
   X,
   Zap,
-  Shield
+  Shield,
+  Settings
 } from 'lucide-react';
 import { useAuth } from '../hooks/useAuth';
 import { useUserSettings } from '../hooks/useUserSettings';
