9dd0d1fa4eef5579bd7682e2df546191bb4da579
motveyich <motveyich@gmail.com>
2026-01-22 16:16:04 +0300

    Updated offers.controller.ts

--- backend/src/modules/profiles/profiles.controller.ts
+++ backend/src/modules/profiles/profiles.controller.ts
@@ -135,7 +135,7 @@ export class ProfilesController {
       new ParseFilePipe({
         validators: [
           new MaxFileSizeValidator({ maxSize: 5 * 1024 * 1024 }), // 5MB
-          new FileTypeValidator({ fileType: /(jpg|jpeg|png|webp)$/ }),
+          new FileTypeValidator({ fileType: /^image\/(jpeg|jpg|png|webp)$/ }),
         ],
       }),
     )
